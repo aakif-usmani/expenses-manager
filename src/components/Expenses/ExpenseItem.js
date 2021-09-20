ff6cf9b45e6f0c1e791bc82d231f987da52d1f6c
Aakif usmani <aakifusmanismas@gmail.com>
2021-09-20 23:07:44 +0500

    init commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ButtonX from "./Button";
+
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated!!!!!");
+    console.log(props.title);
+  };
+
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">Rs={props.amount}/-</div>
+      </div>
+     <ButtonX clickHandler={clickHandler}/>
+     
+    </Card>
+  );
+};
+
+export default ExpenseItem;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import ExpenseForm from "./ExpenseForm"
+import "./NewExpense.css"
+
+const NewExopense = (props) => {
+  const onSaveExpenseDatahandler = (enteredExpenseData) => { 
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  // const onSaveExpenseDatahandler = (enteredExpenseData) => { 
+  //   const expenseData = {
+  //     ...enteredExpenseData,
+  //     id: Math.random().toString()
+  //   };
+  //   console.log(expenseData);
+  // };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData ={onSaveExpenseDatahandler}/>
+      {/* adding prop */}
+    </div>
+  );
+};
+
+export default NewExopense;
